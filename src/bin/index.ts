91f3d39473fe4bc99c13116f39d5df95f1f1ff9f
Evan Darwin <evan@relta.net>
2021-01-21 01:14:09 -0800

    minimist plz?

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -4,6 +4,8 @@ import {testConnections} from "./lib";
 const argv = minimist(process.argv.slice(2))
 const [subcommand] = argv._
 
+declare module "minimist";
+
 function help(): never {
     console.log([
         `expresso - An express framework\n---\n`,
