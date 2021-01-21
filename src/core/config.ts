837d822d14343dd984232fecf463d5c6d3f07824
Evan Darwin <evan@relta.net>
2021-01-21 00:40:48 -0800

    Lint fixes

--- src/core/config.ts
+++ src/core/config.ts
@@ -6,11 +6,11 @@ export interface ExpressoConfiguration<CK = ConfigKeys> {
     __secret: Map<CK, string | boolean | number | undefined>;
 }
 
-export interface EnvConfigData<T = any> {
+export interface EnvConfigData<T = unknown> {
     required?: boolean;
     default?: string | number | boolean;
-    filter?: (value: string | undefined) => T | undefined;
-    verify?: (value: string | T | undefined) => void | never;
+    filter?: (value: unknown) => T | undefined;
+    verify?: (value: T | unknown) => void | never;
 }
 
 export type ConfigKeys = 'APP_SECRET' | 'APP_ENV' | 'APP_DEBUG';
@@ -18,9 +18,9 @@ export type ConfigSetDataSimple = { [key: string]: EnvConfigData };
 export const DefaultConfigSet: { [K in ConfigKeys]: EnvConfigData } = {
     APP_SECRET: <EnvConfigData<string>>{
         required: true,
-        filter: (v) => (v?.toString()),
+        filter: (v) => (`${v || ''}`),
         verify: (secret): void | never => {
-            if (!secret) { // noinspection ExceptionCaughtLocallyJS
+            if (typeof secret !== 'string') { // noinspection ExceptionCaughtLocallyJS
                 throw new Error("Invalid / no value was provided");
             }
 
@@ -35,9 +35,7 @@ export const DefaultConfigSet: { [K in ConfigKeys]: EnvConfigData } = {
     },
     APP_DEBUG: <EnvConfigData<boolean>>{
         default: false,
-        filter: (v) => {
-            return !!(v && (['true', '1'].includes(v.toLowerCase())));
-        }
+        filter: (v) => !!(v && (['true', '1'].includes(`${v}`.toLowerCase()))),
     }
 }
 
@@ -46,29 +44,30 @@ const noop = <R>(s: R): R => s;
 export function readConfiguration<K = ConfigKeys>(customKeys?: { [key: string]: EnvConfigData }): ExpressoConfiguration<K> | never {
     configEnv();
 
-    try {
-        const configMap = new Map<K, unknown>();
-        const log = debug('expresso:config')
-        log('verify environment')
+    const configMap = new Map<K, unknown>();
+    const log = debug('expresso:config')
+    log('verify environment')
 
-        const mergedConfigSet = Object.assign({}, DefaultConfigSet, customKeys || {})
-        for (const [key, data] of Object.entries(mergedConfigSet)) {
-            let envValue = process.env[key];
-            if (data.required && typeof data.default === 'undefined' && typeof envValue === 'undefined') {
-                throw new Error(`You did not provide a value for the '${key}' environment variable, but is required.`)
-            }
-            let res;
-            if (data.default && typeof envValue === 'undefined') {
-                res = data.default;
-            } else {
-                res = (data.filter ? data.filter : noop)(envValue);
-            }
-            configMap.set(key as unknown as K, res)
+    const mergedConfigSet = Object.assign({}, DefaultConfigSet, customKeys || {})
+    for (const [key, data] of Object.entries(mergedConfigSet)) {
+        if (!key.match(/[A-Z\d_]+/)) {
+            throw new Error(`Invalid environment key '${key}'`)
         }
 
-        log('ready')
-        return <ExpressoConfiguration<K>>{__secret: configMap}
-    } catch (e) {
-        throw e;
+        // eslint-disable-next-line security/detect-object-injection
+        const envValue = process.env[key];
+        if (data.required && typeof data.default === 'undefined' && typeof envValue === 'undefined') {
+            throw new Error(`You did not provide a value for the '${key}' environment variable, but is required.`)
+        }
+        let res;
+        if (data.default && typeof envValue === 'undefined') {
+            res = data.default;
+        } else {
+            res = (data.filter ? data.filter : noop)(envValue);
+        }
+        configMap.set(key as unknown as K, res)
     }
+
+    log('ready')
+    return <ExpressoConfiguration<K>>{__secret: configMap}
 }
